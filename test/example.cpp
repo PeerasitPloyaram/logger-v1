b7666275bed4625ee7d96816a747aaa87b4a4498
Peerasit <peerasitearth@outlook.com>
2023-06-06 19:07:52 +0700

    add example file

--- test/example.cpp
+++ test/example.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <string>
 #include "../src/loggerv1.cpp"
 
+using namespace std;
+
 int main(){
-    std::cout << getCurrentTime("now") << '\n';
+    for (int i = 0; i < 200; i++){
+        uint32_t total = (i + 1) * i;
+        Logger(std::to_string(total));
+    }
+    endLogger();
+    
     return 0;
 }
