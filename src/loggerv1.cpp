2855a1753000ce1efe9918361197a183c4c53ea9
Peerasit <peerasitearth@outlook.com>
2023-06-06 19:08:50 +0700

    fix year month and date

--- src/loggerv1.cpp
+++ src/loggerv1.cpp
@@ -1,6 +1,6 @@
 #include <string>
 #include <cstdio>
-#include <iostream>
+#include <fstream>
 
 std::string getCurrentTime(std::string s){
     time_t time_now = time(0);
@@ -8,13 +8,31 @@ std::string getCurrentTime(std::string s){
     char  buf[80];
     tstruct = *localtime(&time_now);
 
-    if(s == "now")
+    if(s == "all")
         strftime(buf, sizeof(buf), "%Y-%m-%d %X", &tstruct);
     else if(s == "date")
         strftime(buf, sizeof(buf), "%Y-%m-%d", &tstruct);
+    else if (s == "year"){
+        strftime(buf,sizeof(buf), "%Y", &tstruct);
+    }
+    else if (s == "month")
+        strftime(buf,sizeof(buf), "%m", &tstruct);
+    
     return  std::string(buf);
 }
 
-void logger(std::string s){
-    
+inline void Logger(std::string logMsg){
+    std::string filePath = "./log_"+getCurrentTime("date")+".txt";
+    std::string now = getCurrentTime("all");
+    std::ofstream ofs(filePath.c_str(), std::ios_base::out | std::ios_base::app );
+    ofs << now << " ❯ " <<logMsg << '\n';
+    ofs.close();
+}
+
+inline void endLogger(){
+    std::string filePath = "./log_"+getCurrentTime("date")+".txt";
+    std::string now = getCurrentTime("all");
+    std::ofstream ofs(filePath.c_str(), std::ios_base::out | std::ios_base::app );
+    ofs << now << " ❯ " << "End log ..." << '\n';
+    ofs.close();
 }
